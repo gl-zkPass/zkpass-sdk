be89bfc53051bd53a8eebecd8b9289de25c9083b
Ardiant Utomo <ardiant.utomo@gdplabs.id>
2023-05-23 16:46:20 +0700

    added get verifier QR api

---
+++ src/pages/api/get-verifier-qr.ts
@@ -0,0 +1,56 @@
+import didPass from "@swiss-web2/utils/didpass";
+import {
+  CredType,
+  IVerifyCriteria,
+  IVerifyRequestReceipt,
+  KycKtpField,
+  VerifyOperator,
+  VerifyRequest,
+} from "didpass";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export type QrData = {
+  data: string;
+  requestId: string;
+};
+
+const criteria: IVerifyCriteria = {
+  credField: KycKtpField.BirthDate,
+  verifyOperator: VerifyOperator.lessThan,
+  value: 20080101,
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<QrData>
+) {
+  if (req.method !== "GET") {
+    res.status(404);
+    return;
+  }
+  const qrData = await getQr();
+
+  res.status(200).json(qrData);
+}
+
+async function getQr() {
+  let criteria: IVerifyCriteria = {
+    credField: KycKtpField.BirthDate,
+  };
+
+  criteria = {
+    ...criteria,
+    verifyOperator: VerifyOperator.lessThan,
+    value: 20080101,
+  };
+
+  const verifyRequest = new VerifyRequest(CredType.Ktp, criteria);
+  const verifyRequestReceipt: IVerifyRequestReceipt =
+    await didPass.requestVerification(verifyRequest);
+  let requestId = verifyRequestReceipt.id as string;
+  const qrData = JSON.stringify(verifyRequestReceipt.qrCode, null, 2);
+  return {
+    requestId,
+    data: qrData,
+  };
+}
