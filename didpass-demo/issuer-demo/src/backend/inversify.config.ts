4aecccd306e6476fe70feb5f9a1bd6bd1fd09b6b
Handrian Alandi <handrianalandi@gdplabs.id>
2023-11-13 15:25:46 +0700

    fix: change redis to lookup table

--- didpass-demo/issuer-demo/src/backend/inversify.config.ts
+++ didpass-demo/issuer-demo/src/backend/inversify.config.ts
@@ -1,16 +1,10 @@
 import { ConnectService } from "./issuer/ConnectService";
 import { Container } from "inversify";
-import { ISessionStorage } from "./storage/ISessionStorage";
-import { RedisSessionStorage } from "./storage/redis/RedisSessionStorage";
 import { IssuerService } from "./issuer/IssuerService";
 
 const container = new Container();
 
 container.bind<ConnectService>("ConnectService").to(ConnectService);
-container
-    .bind<ISessionStorage>("ISessionStorage")
-    .to(RedisSessionStorage)
-    .inSingletonScope();
 
 container
     .bind<IssuerService>("IssuerService")
