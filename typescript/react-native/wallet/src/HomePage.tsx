34c8e20a785cf47d24e5b1787f382fc57a222749
janice-laksana <janice.laksana@gdplabs.id>
2023-11-29 09:52:28 +0700

    refactor homepage

--- typescript/react-native/wallet/src/HomePage.tsx
+++ typescript/react-native/wallet/src/HomePage.tsx
@@ -1,12 +1,12 @@
 /*
  * HomePage.tsx
  * Main Page of The Wallet Demo Mobile App
- * 
+ *
  * Authors:
  *   LawrencePatrickSianto (lawrence.p.sianto@gdplabs.id)
  * Created at: November 7th 2023
  * -----
- * Last Modified: November 29th 2023
+ * Last Modified: November 28th 2023
  * Modified By: LawrencePatrickSianto (lawrence.p.sianto@gdplabs.id)
  * -----
  * Reviewers:
@@ -20,85 +20,82 @@
 
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, Alert } from 'react-native';
+import { View, Text, TouchableOpacity, Clipboard, Alert } from 'react-native';
 import { styles } from './styles';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { 
-  generateZkPassProof 
-} from '@didpass/zkpass-client-react-native';
+import { generateZkPassProof } from '@didpass/zkpass-client-react-native';
 import Config from 'react-native-config';
 import { JwtPayload, decode } from 'jsonwebtoken';
-import Clipboard from '@react-native-clipboard/clipboard';
-import { DownloadDirectoryPath, exists, readFile, unlink, writeFile } from 'react-native-fs';
+import {
+  DownloadDirectoryPath,
+  exists,
+  readFile,
+  unlink,
+  writeFile,
+} from 'react-native-fs';
 import JsonDisplayer from './json-displayer/JsonDisplayer';
 import { StatusCodes } from 'http-status-codes';
 
-enum PAGE_STATE {
+enum PageState {
   DVR,
   UserData,
   GenerateProof,
-  ShowTokenProof
+  ShowTokenProof,
 }
 
 const HomePage = () => {
-  const [pageState, setPageState] = useState<PAGE_STATE>(PAGE_STATE.DVR);
+  const [pageState, setPageState] = useState<PageState>(PageState.DVR);
   const [userData, setUserData] = useState<string>('');
   const [dvr, setDvr] = useState<string>('');
   const [isError, setIsError] = useState<boolean>(false);
   const [proofToken, setProofToken] = useState<string>('');
-  
-  useEffect(() => {
-    if (!Config.USER_DATA_TOKEN || !Config.DVR_TOKEN) {
-      setIsError(true);
-      return;
-    }
-    const userData = decode(Config.USER_DATA_TOKEN!) as JwtPayload;
-    const decodedUserData = userData?.data;
-    setUserData(decodedUserData);
 
-    const Dvr = decode(Config.DVR_TOKEN!) as JwtPayload;
-    const decodedDvr = Dvr?.data;
-    const decodedDvrQuery = JSON.parse(decodedDvr.query);
-    setDvr(decodedDvrQuery);
+  useEffect(() => {
+    const loadInitialData = async () => {
+      if (!Config.USER_DATA_TOKEN || !Config.DVR_TOKEN) {
+        setIsError(true);
+        return;
+      }
+
+      const userDataPayload = decode(Config.USER_DATA_TOKEN) as JwtPayload;
+      setUserData(userDataPayload?.data);
+
+      const dvrPayload = decode(Config.DVR_TOKEN) as JwtPayload;
+      setDvr(JSON.parse(dvrPayload?.data.query));
+    };
+
+    loadInitialData();
   }, []);
 
   const handleCopyPress = () => {
-    if(pageState == PAGE_STATE.DVR) {
-      Clipboard.setString(JSON.stringify(dvr, null, 2));
-    } else if(pageState == PAGE_STATE.UserData) {
-      Clipboard.setString(JSON.stringify(userData, null, 2));
-    }
+    const dataToCopy = pageState === PageState.DVR ? dvr : userData;
+    Clipboard.setString(JSON.stringify(dataToCopy, null, 2));
   };
-  
+
   const goNext = () => {
-    if(pageState == PAGE_STATE.DVR) {
-      setPageState(PAGE_STATE.UserData);
-    } else if(pageState == PAGE_STATE.UserData) {
-      setPageState(PAGE_STATE.GenerateProof);
-    } else if(pageState == PAGE_STATE.GenerateProof) {
-      setPageState(PAGE_STATE.ShowTokenProof);
+    if (pageState == PageState.DVR) {
+      setPageState(PageState.UserData);
+    } else if (pageState == PageState.UserData) {
+      setPageState(PageState.GenerateProof);
+    } else if (pageState == PageState.GenerateProof) {
+      setPageState(PageState.ShowTokenProof);
     }
   };
 
   const goBack = () => {
-    if(pageState == PAGE_STATE.UserData) {
-      setPageState(PAGE_STATE.DVR);
-    } else if(pageState == PAGE_STATE.GenerateProof) {
-      setPageState(PAGE_STATE.UserData);
-    } else if(pageState == PAGE_STATE.ShowTokenProof) {
-      setPageState(PAGE_STATE.GenerateProof);
+    if (pageState == PageState.UserData) {
+      setPageState(PageState.DVR);
+    } else if (pageState == PageState.GenerateProof) {
+      setPageState(PageState.UserData);
+    } else if (pageState == PageState.ShowTokenProof) {
+      setPageState(PageState.GenerateProof);
     }
   };
 
   const goToStart = () => {
-    setPageState(PAGE_STATE.DVR);
+    setPageState(PageState.DVR);
     setProofToken('');
     setIsError(false);
-  };
-
-  const truncateToken = (token:string) => {
-    const TOKEN_LIMIT = 1000;
-    return token.substring(0, TOKEN_LIMIT) + '...';
   };
 
   const downloadProofAsFile = async () => {
@@ -111,8 +108,8 @@ const HomePage = () => {
     writeFile(path, proofToken)
       .then(() => {
         readFile(path)
-          .then(() => {
-            Alert.alert('File Downloaded', 'The proof_token.json is stored in Downloads folder');
+          .then((token) => {
+            Alert.alert('Success Download', token.substring(0, 100) + '...');
           })
           .catch((err) => {
             Alert.alert('Error Reading Saved JSON', err.message);
@@ -122,7 +119,6 @@ const HomePage = () => {
         Alert.alert('Error Downloading Saved JSON', err.message);
       });
   };
-
 
   const generateProof = async () => {
     try {
@@ -144,19 +140,16 @@ const HomePage = () => {
        * Generate ZkPass Proof by ZkPass Url
        */
       const result: any = await generateZkPassProof(
-        zkPassUrl!, 
+        zkPassUrl!,
         userDataToken!,
         dvrToken!
       );
 
       if (result.status == StatusCodes.OK && result.proof) {
-        setPageState(PAGE_STATE.ShowTokenProof);
+        setPageState(PageState.ShowTokenProof);
         setProofToken(result.proof);
-        setIsError(false);     
-      }
-      else {
-        throw new Error('Error Generating Proof');
-      }
+        setIsError(false);
+      } else throw 'error';
     } catch (error) {
       setProofToken('');
       setIsError(true);
@@ -169,7 +162,7 @@ const HomePage = () => {
 
   useEffect(() => {
     (async () => {
-      if(pageState == PAGE_STATE.GenerateProof) {
+      if (pageState == PageState.GenerateProof) {
         await generateProof();
       }
     })();
@@ -177,20 +170,16 @@ const HomePage = () => {
 
   if (isError) {
     return (
-      <SafeAreaView
-        style={styles.safeArea}
-      >
+      <SafeAreaView style={styles.safeArea}>
         <View style={styles.container}>
-          <Text style={{color: 'red'}}>Oops! Something wrong happened!</Text>
+          <Text style={{ color: 'red' }}>Oops! Something wrong happened!</Text>
 
           <View style={styles.buttonContainer}>
             <TouchableOpacity
               style={styles.button}
               onPress={retryGenerateProof}
             >
-              <Text style={styles.buttonText}>
-                Retry
-              </Text>
+              <Text style={styles.buttonText}>Retry</Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -199,30 +188,29 @@ const HomePage = () => {
   }
 
   return (
-    <SafeAreaView
-      style={styles.safeArea}
-    >
+    <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
         <Text style={styles.header}>
-          {pageState == PAGE_STATE.DVR && 'Please review your Data Verification Request'}
-          {pageState == PAGE_STATE.UserData && 'Please review your User Data'}
-          {pageState == PAGE_STATE.GenerateProof && 'Generating Proof'}
-          {pageState == PAGE_STATE.ShowTokenProof && 'Generated Token Proof'}
+          {pageState == PageState.DVR &&
+            'Please review your Data Verification Request'}
+          {pageState == PageState.UserData && 'Please review your User Data'}
+          {pageState == PageState.GenerateProof && 'Generating Proof'}
+          {pageState == PageState.ShowTokenProof && 'Generated Token Proof'}
         </Text>
 
         {/* Display JSON Data & Token */}
-        {(pageState == PAGE_STATE.DVR || 
-          pageState == PAGE_STATE.UserData || 
-          pageState == PAGE_STATE.ShowTokenProof) &&
+        {(pageState == PageState.DVR ||
+          pageState == PageState.UserData ||
+          pageState == PageState.ShowTokenProof) && (
           <View style={styles.dvrInformation}>
             <View style={styles.dvrInformationHeader}>
               <Text style={styles.dvrInformationHeaderText}>
-                {pageState == PAGE_STATE.DVR && 'Data Verification Request'}
-                {pageState == PAGE_STATE.UserData && 'User Data'}
-                {pageState == PAGE_STATE.ShowTokenProof && 'Token Proof'}
+                {pageState == PageState.DVR && 'Data Verification Request'}
+                {pageState == PageState.UserData && 'User Data'}
+                {pageState == PageState.ShowTokenProof && 'Token Proof'}
               </Text>
 
-              {pageState == PAGE_STATE.ShowTokenProof &&  
+              {pageState == PageState.ShowTokenProof && (
                 <TouchableOpacity
                   style={styles.dvrInformationHeaderCopy}
                   onPress={downloadProofAsFile}
@@ -232,67 +220,50 @@ const HomePage = () => {
                     Download
                   </Text>
                 </TouchableOpacity>
-              }
+              )}
 
               <TouchableOpacity
                 style={styles.dvrInformationHeaderCopy}
                 onPress={handleCopyPress}
                 testID={'copy-button'}
               >
-                <Text style={styles.dvrInformationHeaderCopyText}>
-                  Copy
-                </Text>
+                <Text style={styles.dvrInformationHeaderCopyText}>Copy</Text>
               </TouchableOpacity>
             </View>
-            
+
             <JsonDisplayer
               jsonText={
-                pageState == PAGE_STATE.DVR 
+                pageState == PageState.DVR
                   ? JSON.stringify(dvr, null, 2)
-                  : pageState == PAGE_STATE.UserData 
+                  : pageState == PageState.UserData
                     ? JSON.stringify(userData, null, 2)
-                    : truncateToken(proofToken)
+                    : proofToken.substring(0, 1000) + '...'
               }
               containerStyle={styles.dvrContainer}
             />
           </View>
-        }
+        )}
       </View>
 
       {/* Display Navigation Buttons */}
       <View style={styles.buttonContainer}>
-        {pageState == PAGE_STATE.UserData && 
-          <TouchableOpacity
-            style={styles.button}
-            onPress={goBack}
-          >
-            <Text style={styles.buttonText}>
-              Back
-            </Text>
+        {pageState == PageState.UserData && (
+          <TouchableOpacity style={styles.button} onPress={goBack}>
+            <Text style={styles.buttonText}>Back</Text>
           </TouchableOpacity>
-        }
+        )}
 
-        {(pageState == PAGE_STATE.DVR || pageState == PAGE_STATE.UserData) && 
-          <TouchableOpacity
-            style={styles.button}
-            onPress={goNext}
-          >
-            <Text style={styles.buttonText}>
-              Continue
-            </Text>
+        {(pageState == PageState.DVR || pageState == PageState.UserData) && (
+          <TouchableOpacity style={styles.button} onPress={goNext}>
+            <Text style={styles.buttonText}>Continue</Text>
           </TouchableOpacity>
-        }
+        )}
 
-        {pageState == PAGE_STATE.ShowTokenProof && 
-          <TouchableOpacity
-            style={styles.button}
-            onPress={goToStart}
-          >
-            <Text style={styles.buttonText}>
-              Go to Start
-            </Text>
+        {pageState == PageState.ShowTokenProof && (
+          <TouchableOpacity style={styles.button} onPress={goToStart}>
+            <Text style={styles.buttonText}>Go to Start</Text>
           </TouchableOpacity>
-        }
+        )}
       </View>
     </SafeAreaView>
   );
