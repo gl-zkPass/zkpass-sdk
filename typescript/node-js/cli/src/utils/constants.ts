135521b16e0ff2fc236276660bf7339690920b90
handrianalandi <handrian.alandi@gdplabs.id>
2024-03-12 10:17:10 +0700

    fix: revert cli constant to playground

--- typescript/node-js/cli/src/utils/constants.ts
+++ typescript/node-js/cli/src/utils/constants.ts
@@ -7,7 +7,7 @@
  *   NaufalFakhri (naufal.f.muhammad@gdplabs.id)
  * Created Date: November 29th 2023
  * -----
- * Last Modified: March 8th 2024
+ * Last Modified: March 12th 2024
  * Modified By: handrianalandi (handrian.alandi@gdplabs.id)
  * -----
  * Reviewers:
@@ -22,7 +22,7 @@
 import { ZkPassApiKey } from "@didpass/zkpass-client-ts";
 
 // Holder constants
-export const ZKPASS_SERVICE_URL: string = "https://staging-zkpass.ssi.id";
+export const ZKPASS_SERVICE_URL: string = "https://playground-zkpass.ssi.id";
 // export const ZKPASS_SERVICE_URL: string = "http://localhost:10888";
 
 // Issuer constants
@@ -48,8 +48,8 @@ export const VERIFIER_JKU: string =
 export const EXPECTED_DVR_TTL: number = 600;
 
 // API Keys
-export const KEY = "e7fd7ec9-33b2-4f33-a383-c2f1d151a7c2";
-export const SECRET = "6a79ffa2-5fe8-4764-8edf-0ebc5dbcccf9";
+export const KEY = "5ecb2229-ddee-460e-b598-a0001c10fff1";
+export const SECRET = "074a53a8-a252-45de-a9d5-0961a6362df6";
 // export const KEY = "api_1";
 // export const SECRET = "secret_api_1";
 export const API_KEY: ZkPassApiKey = new ZkPassApiKey(KEY, SECRET);
