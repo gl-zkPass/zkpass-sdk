51959413bffbe7a262c22e156062bc3fe723cf57
William Hartanto Hendrawan <william.h.hendrawan@gdplabs.id>
2024-03-14 15:32:27 +0700

    update rust doc

--- docs/rust/implementors/core/marker/trait.StructuralEq.js
+++ docs/rust/implementors/core/marker/trait.StructuralEq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"zkpass_client":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.73.0/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"struct\" href=\"zkpass_client/core/struct.ProofMethodOutput.html\" title=\"struct zkpass_client::core::ProofMethodOutput\">ProofMethodOutput</a>"]]
+"zkpass_client":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.75.0/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"struct\" href=\"zkpass_client/core/struct.ProofMethodOutput.html\" title=\"struct zkpass_client::core::ProofMethodOutput\">ProofMethodOutput</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
