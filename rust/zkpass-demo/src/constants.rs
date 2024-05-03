44a04f9717884d5c56d6508e2ce97879ea849a13
zulamdat <zulchaidir@gdplabs.id>
2024-05-03 16:40:30 +0700

    revert local constant

--- rust/zkpass-demo/src/constants.rs
+++ rust/zkpass-demo/src/constants.rs
@@ -2,6 +2,3 @@
 pub const API_KEY: &str = "e7fd7ec9-33b2-4f33-a383-c2f1d151a7c2";
 pub const SECRET_API_KEY: &str = "6a79ffa2-5fe8-4764-8edf-0ebc5dbcccf9";
 pub const ZKPASS_URL: &str = "https://staging-zkpass.ssi.id";
-// pub const API_KEY: &str = "api_1";
-// pub const SECRET_API_KEY: &str = "secret_api_1";
-// pub const ZKPASS_URL: &str = "http://localhost:10888";
