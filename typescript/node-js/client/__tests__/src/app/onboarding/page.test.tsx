c9c6c613ea95cfeef04e9fce9fbbec3c0be31d4b
LawrencePatrickSianto <lawrence.p.sianto@gdplabs.id>
2024-09-06 13:57:05 +0700

    [client] Add onbboarding/page.tsx unit test

---
+++ typescript/node-js/client/__tests__/src/app/onboarding/page.test.tsx
@@ -0,0 +1,55 @@
+/*
+ * onboarding/page.tsx
+ *
+ * References:
+ *   NONE
+ * ---
+ * Copyright (c) 2023 PT Darta Media Indonesia. All rights reserved.
+ */
+
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { cookies } from "next/headers";
+import Users from "@/app/onboarding/page";
+
+jest.mock("next/headers", () => ({
+  cookies: jest.fn(),
+}));
+
+jest.mock("next/navigation", () => ({
+  useRouter: jest.fn().mockReturnValue({ push: jest.fn() }),
+  useSearchParams: jest.fn().mockReturnValue({
+    get: jest.fn().mockReturnValue("multiple"),
+  }),
+}));
+
+const mockFetch = jest.fn();
+global.fetch = mockFetch;
+
+describe("Onboarding Page", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("renders EmployeeOnboarding with user when cookie is present", () => {
+    (cookies as jest.Mock).mockReturnValue({
+      get: jest.fn().mockReturnValue({ name: "username", value: "testuser" }),
+    });
+
+    render(<Users />);
+    expect(
+      screen.getByText("Testuser's Employee Onboarding.")
+    ).toBeInTheDocument();
+  });
+
+  test("renders EmployeeOnboarding without user when cookie is not present", () => {
+    (cookies as jest.Mock).mockReturnValue({
+      get: jest.fn().mockReturnValue(undefined),
+    });
+
+    render(<Users />);
+    expect(
+      screen.queryByText("Testuser's Employee Onboarding.")
+    ).not.toBeInTheDocument();
+  });
+});
