667e2d2d38676949d33a9b4490319d0acab7ba0e
LawrencePatrickSianto <lawrence.p.sianto@gdplabs.id>
2024-09-09 09:20:47 +0700

    Add test initialization

---
+++ typescript/node-js/issuer-verifier/jest.config.ts
@@ -0,0 +1,30 @@
+import { Config } from "jest";
+
+const config: Config = {
+  automock: false,
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!**/node_modules/**",
+    "!**/vendor/**",
+  ],
+  coverageDirectory: "coverage",
+  coveragePathIgnorePatterns: ["/node_modules/"],
+  moduleNameMapper: {
+    "^@/(.*)$": ["<rootDir>/src/$1"],
+    "^@components/(.*)$": ["<rootDir>/src/components/$1"],
+    // "\\.(css|less|sass|scss)$": "identity-obj-proxy", //TODO
+    // "^jose": require.resolve("jose"),
+  },
+  // modulePathIgnorePatterns: ["<rootDir>/__tests__/test-utils"],
+  preset: "ts-jest",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { configFile: "next/babel" }],
+  },
+  transformIgnorePatterns: ["/node_modules/", "\\.pnp\\.[^\\/]+$"],
+};
+
+export default config;
